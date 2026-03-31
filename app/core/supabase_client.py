7a4bba7127adabd5d513ac10d2c32bc302c59ae0
PiyushLadukar <piyushladukar2@gmail.com>
2026-03-31 13:16:54 +0530

    user data with onboarding process shown in db

--- app/core/supabase_client.py
+++ app/core/supabase_client.py
@@ -1,11 +1,14 @@
 from supabase import create_client
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 print("SUPABASE_URL:", SUPABASE_URL)
-print("SUPABASE_KEY:", SUPABASE_KEY)
+print("SUPABASE_KEY:", "SET" if SUPABASE_KEY else None)
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise Exception("❌ Supabase ENV missing")
